1f24016d0c94d74c0f1401fb9172340df87bcbcb
Adam <ramirez@blindside.pro>
2022-03-08 09:35:15 +0100

    Changed moralis API key

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const theme = extendTheme({
   },
 });
 
-const moralisAppId = "77IfzDQc5dLFD3YbNKeuNrxHGe2c5OWGoMxgfLTU";
-const moralisServerURL = "https://ioc7ccqovdu5.grandmoralis.com:2053/server";
+const moralisAppId = "g1jI6CNnOfl0PeP2PO9QvzDQ0OO2jwvGWVLo67I9";
+const moralisServerURL = "https://ayqzhdaixlov.usemoralis.com:2053/server";
 
 ReactDOM.render(
   <React.StrictMode>
